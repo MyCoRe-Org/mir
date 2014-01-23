b48dd691c7d23d8afcf24103cefdb41e78696353
Thomas Scheffler <github@yagee.de>
2014-01-23 13:32:10 +0000

    added setup scripts
    git-svn-id: https://server.mycore.de/svn/maven/mir/trunk@28904 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mir-cli/src/main/bin/setup.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# This file is part of "MIR (MyCoRe/MODS Institutional Repository)"
+# See http://www.mycore.de/generated/mir/ for details.
+#
+# GNU General Public License (GPL) version 3.0
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/gpl-3.0.html>.
+
+
+# resolve links - $0 may be a softlink
+PRG="$0"
+
+while [ -h "$PRG" ]; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
+PRGDIR=`dirname "$PRG"`
+BASEDIR=`cd "$PRGDIR/.." >/dev/null; pwd`
+${PRGDIR}/mir.sh process ${BASEDIR}/config/setup-commands.txt
