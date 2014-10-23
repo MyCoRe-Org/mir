e1ec9f7edbb313fcf3b48b74bfa6511c8f969f3d
René Adler <adlerre@users.noreply.github.com>
2014-10-23 11:21:59 +0000

    - improve search person plugin - remove unneeded styles
    git-svn-id: https://server.mycore.de/svn/maven/mir/trunk@30935 7089d063-bc14-496d-ac90-53de1872eb6c

--- mir-module/src/main/resources/META-INF/resources/js/jquery.search-person.js
+++ mir-module/src/main/resources/META-INF/resources/js/jquery.search-person.js
@@ -39,25 +39,70 @@
 		this.$searchBtn = null;
 		this.$typeBtn = null;
 		this.$typeMenu = null;
+		this.$feedback = null;
 
-		this.selectedType = SearchPerson.DEFAULTS.searchType;
+		this.selectedType = options.searchType.toUpperCase() == "SELECT" ? SearchPerson.DEFAULTS.searchType : options.searchType;
 
 		this.init();
 	};
 
 	SearchPerson.VERSION = '1.0.0';
 
-	SearchPerson.TYPES = [ "GND", "VIAF" ];
+	SearchPerson.LABEL_CLEANUP = /\s[\(]?[0-9-]+[\)]?/g;
+
+	SearchPerson.TYPES = {
+		GND : {
+			url : "http://lobid.org/person",
+			data : function(input) {
+				return {
+					name : input,
+					format : "ids"
+				}
+			},
+			dataType : "jsonp",
+			baseURI : "http://d-nb.info/gnd/",
+		},
+		VIAF : {
+			url : "http://www.viaf.org/viaf/AutoSuggest",
+			data : function(input) {
+				return {
+					query : input,
+				}
+			},
+			dataType : "jsonp",
+			dataConvert : function(data) {
+				var result = [];
+				if (data.result !== undefined) {
+					$(data.result).each(function(index, item) {
+						if (item.nametype.toLowerCase() === "personal") {
+							var person = {
+								label : item.term,
+								value : "http://www.viaf.org/viaf/" + item.viafid
+							};
+							result.push(person);
+						}
+					});
+				}
+				return result;
+			},
+			baseURI : "http://www.viaf.org/viaf/"
+		}
+	};
 
 	SearchPerson.DEFAULTS = {
 		// Button Style
 		buttonClass : "btn btn-default",
+		// Label Style (optical feedback for current selection)
+		labelClass : "label label-primary",
 
 		// min length of search term
 		inputMinLength : 3,
 
 		// default search type
 		searchType : "GND",
+
+		// the max. height of results container
+		searchResultMaxHeight : 200,
 
 		// TEXT DEFINITIONS
 		// ===============================
@@ -104,6 +149,12 @@
 		$actions.append($searchBtn);
 
 		if (options.searchType.toUpperCase() == "SELECT") {
+			// preselect searchType by given output value
+			var outputVal = $(options.searchOutput).val();
+			if (outputVal.length > 0) {
+				this.selectedType = getTypeFromURL(outputVal);
+			}
+
 			var $typeBtn = this.$typeBtn = $(document.createElement("button"));
 			$typeBtn.addClass(options.buttonClass).addClass("dropdown-toggle");
 			$typeBtn.attr("data-toggle", "dropdown");
@@ -115,15 +166,19 @@
 			$typeMenu.addClass("dropdown-menu dropdown-menu-right");
 			$typeMenu.attr("role", "menu");
 
-			for ( var index in SearchPerson.TYPES) {
+			for ( var type in SearchPerson.TYPES) {
 				var $entry = $(document.createElement("li"));
+				(type.toUpperCase() == this.selectedType.toUpperCase()) && $entry.addClass("active");
+
 				var $ea = $(document.createElement("a"));
 				$ea.attr("href", "#");
-				$ea.data("search-type", SearchPerson.TYPES[index]);
-				$ea.text(SearchPerson.TYPES[index]);
+				$ea.data("search-type", type);
+				$ea.text(type);
 
 				$ea.on("click", function(e) {
 					that.selectedType = $(this).data("search-type");
+					$(".active", $(this).parents("ul")).toggleClass("active");
+					$(this).parent().toggleClass("active");
 				});
 
 				$entry.append($ea);
@@ -136,6 +191,8 @@
 
 		$inputGroup.append($actions);
 		$parent.append($inputGroup);
+
+		this.updateOutput();
 	}
 
 	SearchPerson.prototype.search = function(e) {
@@ -155,21 +212,27 @@
 			if (input.length < options.inputMinLength)
 				return;
 
+			var type = null;
+			for ( var t in SearchPerson.TYPES) {
+				if (this.selectedType.toUpperCase() == t.toUpperCase()) {
+					type = SearchPerson.TYPES[t];
+					break;
+				}
+			}
+
 			this.$searchBtn.button("loading");
-			if (this.selectedType.toUpperCase() === "GND") {
-				SearchPerson.searchGND(input, function(data) {
+			if (type != null) {
+				SearchPerson.loadData(type.url, type.dataType, type.data(input), function(data) {
 					if (data !== undefined) {
-						that.showResult(data);
+						that.showResult(SearchPerson.sortData(input, typeof type.dataConvert == "function" ? type.dataConvert(data) : data));
+					} else {
+						that.showResult();
 					}
 					that.$searchBtn.button("reset");
-				});
-			} else if (this.selectedType.toUpperCase() === "VIAF") {
-				SearchPerson.searchVIAF(input, function(data) {
-					if (data !== undefined) {
-						that.showResult(data);
-					}
+				}, function() {
+					that.showResult();
 					that.$searchBtn.button("reset");
-				});
+				})
 			} else {
 				console.error("Search type \"" + this.selectedType.toUpperCase() + "\" is unsupported!");
 				this.$searchBtn.button("reset");
@@ -184,13 +247,11 @@
 		var $parent = this.$parent;
 		var options = this.options;
 
-		var $output = $(options.searchOutput, getParent(this.$element))[0] !== undefined ? $(options.searchOutput, getParent(this.$element)) : this.$element;
-
 		var $resultBox = $(document.createElement("ul"));
 		$resultBox.attr("role", "menu");
 		$resultBox.addClass("dropdown-menu");
 
-		if (data.length > 0) {
+		if (data && data.length > 0) {
 			$(data).each(function(index, item) {
 				var $li = $(document.createElement("li"));
 
@@ -198,7 +259,7 @@
 				$person.attr("href", "#");
 				$person.html(item.label);
 				$person.on("click", function(e) {
-					$output.val(item.value);
+					that.updateOutput(item);
 					that.clearAll();
 				});
 
@@ -213,11 +274,53 @@
 		}
 
 		$parent.append($resultBox);
+		$resultBox.css({
+			height : "auto",
+			maxHeight : options.searchResultMaxHeight,
+			width : "100%",
+			overflow : "auto",
+			overflowX : "hidden"
+		});
 		$resultBox.dropdown("toggle");
 
 		this.$element.data($.extend({}, {
 			searchResultContainer : $resultBox
 		}, options));
+	}
+
+	SearchPerson.prototype.updateOutput = function(item) {
+		var options = this.options;
+		var $output = $(options.searchOutput, getParent(this.$element))[0] !== undefined ? $(options.searchOutput, getParent(this.$element)) : this.$element;
+
+		if (item) {
+			this.$element != $output && this.$element.val(item.label.replace(SearchPerson.LABEL_CLEANUP, ""));
+			$output.val(item.value);
+		}
+
+		if ($output != this.$element && $output.val().length > 0) {
+			var $feedback = $(document.createElement("a"));
+			$feedback.attr("href", $output.val());
+			$feedback.attr("target", "_blank");
+
+			var $label = $(document.createElement("span"));
+			$label.addClass(options.labelClass);
+			$label.text(getTypeFromURL($output.val()));
+
+			$feedback.append($label);
+
+			if (this.$feedback)
+				this.$feedback.remove();
+
+			this.$feedback = $feedback;
+			this.$element.after($feedback);
+
+			$feedback.css({
+				position : "absolute",
+				marginLeft : -($feedback.width() + 10),
+				marginTop : Math.floor((this.$element.innerHeight() - $feedback.height()) / 2),
+				zIndex : 100
+			});
+		}
 	}
 
 	SearchPerson.prototype.clearAll = function(e) {
@@ -243,52 +346,17 @@
 		});
 	}
 
-	SearchPerson.searchGND = function(input, callback) {
+	SearchPerson.loadData = function(url, dataType, data, successCB, errorCB) {
 		$.ajax({
-			url : "http://lobid.org/person",
+			url : url,
 			timeout : 5000,
-			dataType : "jsonp",
-			data : {
-				name : input,
-				format : "ids"
-			},
+			dataType : dataType,
+			data : data,
 			success : function(data) {
-				callback(SearchPerson.sortData(input, data));
+				successCB(data);
 			},
 			error : function() {
-				callback();
-			}
-		});
-	}
-
-	SearchPerson.searchVIAF = function(input, callback) {
-		$.ajax({
-			url : "http://www.viaf.org/viaf/AutoSuggest",
-			timeout : 5000,
-			dataType : "jsonp",
-			data : {
-				query : input,
-			},
-			success : function(data) {
-				if (data.result !== undefined) {
-					var result = [];
-					$(data.result).each(function(index, item) {
-						if (item.nametype.toLowerCase() === "personal") {
-							var person = {
-								label : item.term,
-								value : "http://www.viaf.org/viaf/" + item.viafid
-							};
-							result.push(person);
-						}
-					});
-
-					callback(SearchPerson.sortData(input, result));
-				} else {
-					callback();
-				}
-			},
-			error : function() {
-				callback();
+				errorCB();
 			}
 		});
 	}
@@ -339,9 +407,18 @@
 		}
 
 		return data.sort(function(a, b) {
-			return levenshteinDistance(input, a.label.replace(/\s[\(]?[0-9-]+[\)]?/g, ""))
-					- levenshteinDistance(input, b.label.replace(/\s[\(]?[0-9-]+[\)]?/g, ""));
+			return levenshteinDistance(input, a.label.replace(SearchPerson.LABEL_CLEANUP, ""))
+					- levenshteinDistance(input, b.label.replace(SearchPerson.LABEL_CLEANUP, ""));
 		});
+	}
+
+	function getTypeFromURL(url) {
+		for ( var type in SearchPerson.TYPES) {
+			if (url.indexOf(SearchPerson.TYPES[type].baseURI) != -1)
+				return type;
+		}
+
+		return null;
 	}
 
 	function getParent($this) {
