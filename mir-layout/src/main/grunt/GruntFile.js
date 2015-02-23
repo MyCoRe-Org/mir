cce37330bb2e6b71774a12ff85bfafa6f8101db1
Thomas Scheffler <github@yagee.de>
2015-02-23 13:42:26 +0000

    MIR-121 #comment update to bootswatch 3.3.2 and resolve issue
    requires node.js 0.12
    
    git-svn-id: https://server.mycore.de/svn/maven/mir/trunk@31950 7089d063-bc14-496d-ac90-53de1872eb6c

--- mir-layout/src/main/grunt/GruntFile.js
+++ mir-layout/src/main/grunt/GruntFile.js
@@ -1,17 +1,21 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-replace');
   grunt.loadNpmTasks('grunt-contrib-watch');
   var fs = require('fs');
+  var path = require('path');
+  var util = require('util');
+  var getAbsoluteDir = function(dir) {
+    return path.isAbsolute(dir) ? dir : path.resolve(process.cwd(), dir);
+  }
   var globalConfig = {
-    lessFile : grunt.option('lessFile'),
-    lessDirectory : function() {
-      var lessFile = grunt.config('globalConfig.lessFile');
-      return lessFile.substring(0, Math.max(lessFile.lastIndexOf("/"), lessFile.lastIndexOf("\\")));
-    },
-    targetDirectory : grunt.option('targetDirectory'),
-    lastModified:new Date(0)
+    lessDirectory : getAbsoluteDir(path.join('..', '..', 'src', 'main', 'less')),
+    targetDirectory : getAbsoluteDir(grunt.option('targetDirectory')),
+    layouts : {
+      "cosmol" : {},
+      "flatmir" : {}
+    }
   };
   var dirLastModified = function(dir, date) {
     var src = grunt.file.expand(dir + '/**/*.less');
@@ -27,166 +31,183 @@ module.exports = function (grunt) {
       grunt.file.write(filepath);
     }
   };
-  var needRebuild=function(dest){
+  var needRebuild = function(srcModified, dest) {
     var destModified = fs.existsSync(dest) ? fs.statSync(dest).mtime : new Date(0);
-    var srcModified = grunt.config('globalConfig.lastModified');
     return srcModified.getTime() > destModified.getTime();
-  }
-
-  grunt.initConfig({
-    globalConfig: globalConfig,
-    pkg: grunt.file.readJSON('package.json'),
-    bootstrap: grunt.file.readJSON('bower_components/bootstrap/package.json'),
-    banner: '/*!\n' +
-            ' * <%= pkg.name %> v${project.version}\n' +
-            ' * Homepage: <%= pkg.homepage %>\n' +
-            ' * Copyright 2013-<%= grunt.template.today("yyyy") %> <%= pkg.author %> and others\n' +
-            ' * Licensed under <%= pkg.license %>\n' +
-            ' * Based on Bootstrap and Bootswatch\n' +
-            '*/\n',
-    mir: {
-      cerulean:{}, cosmo:{}, cyborg:{}, "default":{},
-      darkly:{}, flatly:{}, journal:{}, lumen:{}, readable:{},
-      sandstone:{}, simplex:{}, slate:{}, spacelab:{}, superhero:{},
-      united:{}, yeti:{}
-    },
-    replace: {
-      dist: {
-        options: {
-          patterns:[
-            // resolve css from google fonts on build time
-            {
-              match: /@import url\("\/\/fonts/,
-              replacement: '@import (inline) url("http://fonts'
-            }
-          ]
-        },
-        files: [
-           {
-             expand: true,
-             // flatten: true,
-             src: ['bower_components/bootswatch/*/bootswatch.less'],
-             // dest: 'build/'
-           }
-        ]
+  };
+  var updateLayout = function(layout) {
+    if (layout == undefined) {
+      for ( var layout in grunt.config('globalConfig').layouts) {
+        updateLayout(layout);
       }
-    },
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: false
-      },
-      dist: {
-        src: [],
-        dest: ''
-      }
-    },
-    less: {
-      dist: {
-        options: {
-          compress: false,
-          cleancss: false,
-          ieCompat: false,
-          sourceMap: true,
-          sourceMapURL: "",
-          sourceMapFilename: "",
-          outputSourceFiles: true,
-          modifyVars: {
-            bootswatch_theme: "default",
-            "icon-font-path": "'//netdna.bootstrapcdn.com/bootstrap/<%= bootstrap.version %>/fonts/'",
-          }
-        },
-        files: {}
-      }
-    },
-    watch: {
-      styles: {
-        files: ['../../src/main/less/**/*.less'], // which files to watch
-        tasks: ['flatlyBuild'],
-      }
+      console.log(util.inspect(globalConfig.layouts));
     }
-  });
-  grunt.registerTask('none', function() {});
-  grunt.registerTask('build', 'build a regular theme', function(theme, compress) {
-    var target=grunt.config('globalConfig.targetDirectory')+'/'+theme+'.css';
-    if (needRebuild(target)){
+    var layoutDir = path.resolve(grunt.config('globalConfig.lessDirectory'), layout);
+    var layoutInfo = {
+      "srcDir" : layoutDir,
+      lastModified : dirLastModified(layoutDir, dirLastModified('bower_components'))
+    }
+    globalConfig.layouts[layout] = layoutInfo;
+  };
+
+  var compileLess = function(layout, theme, compress) {
+    var themePrefix = grunt.config('globalConfig.targetDirectory') + '/' + layout + '/' + theme;
+    var target = themePrefix + '.css';
+    var lessSrc = [ '<%=globalConfig.lessDirectory%>' + '/' + layout + '/build.less' ];
+    var srcModified = grunt.config('globalConfig.layouts')[layout].lastModified;
+    if (needRebuild(srcModified, target)) {
       var compress = compress == undefined ? true : compress;
 
       var concatSrc;
       var concatDest;
       var lessDest;
-      var lessSrc;
       var files = {};
       var dist = {};
       lessDest = 'build.css';
-      lessSrc = [ '<%=globalConfig.lessFile%>' ];
       concatSrc = lessDest;
-      concatDest = '<%=globalConfig.targetDirectory%>/'+ theme + '.css';
-      dist = {src: concatSrc, dest: concatDest};
+      concatDest = themePrefix + '.css';
+      dist = {
+        src : concatSrc,
+        dest : concatDest
+      };
       grunt.config('concat.dist', dist);
 
-      files = {}; files[lessDest] = lessSrc;
+      files = {};
+      files[lessDest] = lessSrc;
       grunt.config('less.dist.files', files);
       grunt.config('less.dist.options.compress', false);
       grunt.config('less.dist.options.cleancss', false);
       grunt.config('less.dist.options.modifyVars.bootswatch_theme', theme);
       grunt.config('less.dist.options.sourceMap', true);
       grunt.config('less.dist.options.sourceMapURL', theme + '.css.map');
-      grunt.config('less.dist.options.sourceMapFilename', '<%=globalConfig.targetDirectory%>/' + theme + '.css.map');
+      grunt.config('less.dist.options.sourceMapFilename', themePrefix + '.css.map');
       grunt.log.writeln('compiling file ' + lessSrc + ' ==> ' + lessDest);
 
-      grunt.task.run(['less:dist', 'concat',
-                      compress ? 'compress:'+concatDest+':'+'<%=globalConfig.targetDirectory%>/' + theme + '.min.css':'none']);
+      grunt.task.run([ 'less:dist', 'concat',
+          compress ? 'compress:' + concatDest + ':' + themePrefix + '.min.css' : 'none' ]);
     } else {
-      grunt.log.writeln('do not need to rebuild '+target);
+      grunt.log.writeln('do not need to rebuild ' + target);
     }
+  };
+
+  grunt.initConfig({
+    globalConfig : globalConfig,
+    pkg : grunt.file.readJSON('package.json'),
+    bootstrap : grunt.file.readJSON('bower_components/bootstrap/package.json'),
+    banner : '/*!\n' + ' * <%= pkg.name %> v${project.version}\n' + ' * Homepage: <%= pkg.homepage %>\n'
+        + ' * Copyright 2013-<%= grunt.template.today("yyyy") %> <%= pkg.author %> and others\n'
+        + ' * Licensed under <%= pkg.license %>\n' + ' * Based on Bootstrap and Bootswatch\n' + '*/\n',
+    mir : {
+      cerulean : {},
+      cosmo : {},
+      cyborg : {},
+      "default" : {},
+      darkly : {},
+      flatly : {},
+      journal : {},
+      lumen : {},
+      paper: {},
+      readable : {},
+      sandstone : {},
+      simplex : {},
+      slate : {},
+      spacelab : {},
+      superhero : {},
+      united : {},
+      yeti : {}
+    },
+    replace : {
+      dist : {
+        options : {
+          patterns : [
+          // resolve css from google fonts on build time
+          {
+            match : /@import url\("\/\/fonts/,
+            replacement : '@import (inline) url("http://fonts'
+          } ]
+        },
+        files : [ {
+          expand : true,
+          // flatten: true,
+          src : [ 'bower_components/bootswatch/*/bootswatch.less' ],
+        // dest: 'build/'
+        } ]
+      }
+    },
+    concat : {
+      options : {
+        banner : '<%= banner %>',
+        stripBanners : false
+      },
+      dist : {
+        src : [],
+        dest : ''
+      }
+    },
+    less : {
+      dist : {
+        options : {
+          compress : false,
+          cleancss : false,
+          ieCompat : false,
+          sourceMap : true,
+          sourceMapURL : "",
+          sourceMapFilename : "",
+          outputSourceFiles : true,
+          modifyVars : {
+            bootswatch_theme : "default",
+            "icon-font-path" : "'//netdna.bootstrapcdn.com/bootstrap/<%= bootstrap.version %>/fonts/'",
+          }
+        },
+        files : {}
+      }
+    },
+    watch : {
+      styles : {
+        files : [ '../../src/main/less/**/*.less' ], // which files to watch
+        tasks : [ 'reBuild' ],
+      }
+    }
+  });
+  grunt.registerTask('none', function() {
+  });
+  grunt.registerTask('build', 'build a regular theme', function(layout, theme, compress) {
+    compileLess(layout, theme, compress);
   });
 
   grunt.registerTask('compress', 'compress a generic css', function(fileSrc, fileDst) {
-    var files = {}; files[fileDst] = fileSrc;
+    var files = {};
+    files[fileDst] = fileSrc;
     grunt.log.writeln('compressing file ' + fileSrc);
 
     grunt.config('less.dist.files', files);
     grunt.config('less.dist.options.compress', true);
     grunt.config('less.dist.options.cleancss', true);
     grunt.config('less.dist.options.sourceMap', false);
-    grunt.task.run(['less:dist']);
+    grunt.task.run([ 'less:dist' ]);
   });
 
   grunt.registerMultiTask('mir', 'build a theme', function() {
     var t = this.target;
-    grunt.task.run('build:'+t);
+
+    for ( var layout in grunt.config('globalConfig').layouts) {
+      grunt.task.run('build:' + layout + ":" + t);
+    }
   });
 
   grunt.registerTask('default', 'build a theme', function() {
-    grunt.log.writeln('less directory: '+grunt.config('globalConfig').lessDirectory());
+    grunt.log.writeln('less directory: ' + grunt.config('globalConfig').lessDirectory);
     grunt.task.run('replace');
-    grunt.config(
-        'globalConfig.lastModified',
-        new Date(
-            Math.max(
-                dirLastModified(grunt.config('globalConfig').lessDirectory()),
-                dirLastModified('bower_components')
-            )
-        )
-    );
+    createFileIfNotExist('bower_components/bootswatch/default/variables.less');
+    createFileIfNotExist('bower_components/bootswatch/default/bootswatch.less');
+    updateLayout();
+    grunt.task.run('mir');
+  });
+
+  grunt.registerTask('reBuild', 'rebuild files if necessary', function() {
+    updateLayout();
     createFileIfNotExist('bower_components/bootswatch/default/variables.less');
     createFileIfNotExist('bower_components/bootswatch/default/bootswatch.less');
     grunt.task.run('mir');
   });
-
-  grunt.registerTask('flatlyBuild', 'build flatly theme incl. config', function() {
-      grunt.config(
-          'globalConfig.lastModified',
-          new Date(
-              Math.max(
-                  dirLastModified(grunt.config('globalConfig').lessDirectory()),
-                  dirLastModified('bower_components')
-              )
-          )
-      );
-      createFileIfNotExist('bower_components/bootswatch/default/variables.less');
-      createFileIfNotExist('bower_components/bootswatch/default/bootswatch.less');
-      grunt.task.run('mir');
-    });
 }
