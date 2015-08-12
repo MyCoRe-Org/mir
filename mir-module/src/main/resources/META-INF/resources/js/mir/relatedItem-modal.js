8898bb6eb0c5fb19529882226723913823ab3245
Alexander Miller <AMill7@users.noreply.github.com>
2015-08-12 11:17:40 +0000

    MIR-140 #comment added sort by typ options; result preview show now the typ; fixed bug with pager
    git-svn-id: https://server.mycore.de/svn/maven/mir/trunk@33164 7089d063-bc14-496d-ac90-53de1872eb6c

--- mir-module/src/main/resources/META-INF/resources/js/mir/relatedItem-modal.js
+++ mir-module/src/main/resources/META-INF/resources/js/mir/relatedItem-modal.js
@@ -9,20 +9,28 @@ $﻿(".mir-relatedItem-select").ready(function() {
 	
 	function workRelatedItem(button){
 		initBody();
+		
 		var input = button.next().next("input");
+		var sortType = "";
+		var i18n = [];
+		
+		//load i18nkeys
+		loadPublikation(function(data){i18n = data},
+				"servlets/MCRLocaleServlet/" + $("html.no-js").attr("lang") + "/component.mods.genre.*", 
+				"", "", "JSON");
+		
 		if(input.val().length > 0) {
-			console.log("next input");
-			loadPublikation(leftContent, "", input.val(), "0");
+			loadPublikation(leftContent, "", input.val(), "0", "xml");
 			setTimeout(function() {
 				$("#modalFrame").find(".list-group-item").addClass("active");
 			}, 300);
-			loadPublikation(rightContent, "receive/" + input.val(), "", "");
+			loadPublikation(rightContent, "receive/" + input.val(), "", "", "html");
 			$("#modalFrame-send").removeAttr("disabled");
 		}
 		
+    
 		if(!input.val()) {
-			console.log("normal load");
-			loadPublikation(leftContent, "", "", "0");
+			loadPublikation(leftContent, "", "", "0", "xml");
 		}
 		
 		function initBody() {
@@ -32,8 +40,10 @@ $﻿(".mir-relatedItem-select").ready(function() {
 			$("#modalFrame-body").append("<div id='main_left_content' class='list-group col-md-4' />");
 			$("#modalFrame-body").append("<div id='main_right_content' class='list-group col-md-8' />");
 			$("#main_left_content, #main_right_content").css({"max-height": "560px", "overflow": "auto"});
+			$("#main_right_content").css("padding-left", "10px")
 			//create pager
 			$("#modalFrame-body").after("<nav class='col-md-4' style='clear: both'><ul class='pager'><li id='first' class='previous disabled'><a data='0'>First</a></li><li id='previous' class='previous disabled'><a>Previous</a></li><li class='next disabled'><a>Next</a></li></ul></nav>");
+			$("#modalFrame-cancel").before("<div class='col-md-4'><select class='form-control'><option value=''>Sortieren nach Typ:</option></select></div>");
 			$("li a").css("cursor", "pointer");
 			$("#modal-searchInput").removeAttr("hidden");
 			$("#modalFrame").modal("show");
@@ -45,42 +55,56 @@ $﻿(".mir-relatedItem-select").ready(function() {
 			mainBody.children().each(function() {
 				var autorContainer = "";
 				var autor = "";
-				console.log($(this).find("arr[name='mods.author'] > str").text());
 				$(this).find("arr[name='mods.author'] > str").each(function() {
 					autor = autor + "; " + $(this).text();
 				});
 				autor = $.trim(autor.substring(1, autor.length));
-				console.log("Autor = " + autor);
 				if(autor != "") {
 					autorContainer = "<br/><i><small>Autor: " + autor + "</small></i>"
 				}
-				$("#main_left_content").append("<a class='list-group-item' value='" + $(this).find("str[name='groupValue']").text() + "'>" + $(this).find("str[name='mods.title.main']").text() + autorContainer + "</a>").css("cursor", "pointer");
+				var type = "<br/><i><small>Type: " + i18n["component.mods.genre." + $(this).find("str[name='mods.type']").text()] + "</small></i>"
+				$("#main_left_content").append("<a class='list-group-item' value='" + $(this).find("str[name='groupValue']").text() + "'>" + $(this).find("str[name='mods.title.main']").text() + autorContainer + type + "</a>").css("cursor", "pointer");
 			});
 			
 			updatePager(data);
+			updateType(data);
 		}
 		
 		function rightContent(data) {
 			$("#main_right_content").empty().append($(data).find("h1[itemprop='name']").css("margin-top", "0"));
 			$("h1[itemprop='name'").after($(data).find(".mods_genre").removeAttr("href"));
-			$("#main_right_content").append($(data).find("#main_col").removeClass("col-md-8"));
+			$("#main_right_content").append($(data).find("#main_col > .detail_block:first-child")).append($(data).find(".mir_metadata"));
 			$("a[itemprop='creator']").removeAttr("href");
+		}
+		
+		function updateType(data) {
+			$(".modal-footer select > option[value != '']").remove();
+			$(data).find("lst[name='facet_counts'] lst[name='mods.type'] > int").each(function() {
+				var type_val = encodeURIComponent('+mods.type:"' + $(this).attr('name') + '"');
+				var text = i18n["component.mods.genre." + $(this).attr('name')];
+				$(".modal-footer select").append("<option value='" + type_val + "'>" + text + " (" + $(this).text() + ")</option>");
+			});
+			$(".modal-footer select").val(encodeURIComponent(sortType));
 		}
 		
 		function updatePager(data) {
 			var start = $(data).find("str[name='start']").text();
 			var rows = $(data).find("str[name='rows']").text();
-			var matches = $(data).find("int[name='matches']").text();
+			var matches = $(data).find("int[name='ngroups']").text();
+			
 			$("#previous, li.next, #first").show();
 			$("ul.pager li").removeClass("disabled");
+			
 			if(parseInt(start) - parseInt(rows) < 0) {
 				$("#previous").addClass("disabled");
 			}
 			$("#previous a").attr("data", parseInt(start) - parseInt(rows));
+			
 			if((parseInt(start) + parseInt(rows)) > parseInt(matches)) {
 				$("li.next").addClass("disabled");
 			}
 			$("li.next a").attr("data", parseInt(start) + parseInt(rows));
+			
 			if(parseInt(start) == 0) {
 				$("#first").addClass("disabled");
 			}
@@ -93,7 +117,7 @@ $﻿(".mir-relatedItem-select").ready(function() {
 		$("#modalFrame").on("click", "#main_left_content > .list-group-item", function() {
 			if(!$(this).is($(".list-group-item.active"))) {
 				$(".list-group-item").removeClass("active");
-				loadPublikation(rightContent, "receive/" + $(this).attr("value"), "", "");
+				loadPublikation(rightContent, "receive/" + $(this).attr("value"), "", "", "html");
 				$(this).addClass("active");
 				$("#modalFrame-send").removeAttr("disabled");
 			}
@@ -109,36 +133,45 @@ $﻿(".mir-relatedItem-select").ready(function() {
 			$("#modalFrame-body").empty();
 			$("nav.col-md-4").remove();
 			$("#modal-searchInput > input").val("");
+			$(".modal-footer div.col-md-4").remove();
 		});
 		
 		$("#modal-searchInput > input").unbind().keyup(function(event) {
 			if(event.keyCode == '13') {
-				loadPublikation(leftContent, "", $(this).val(), "0");
 				$("#main_right_content").empty();
+				sortType = "";
+				loadPublikation(leftContent, "", $(this).val(), "0", "xml");
 			}
 		});
 		
-		$(".glyphicon-search").unbind().click(function() {
+		$("#modal-searchInput .glyphicon-search").unbind().click(function() {
 			$("#main_right_content").empty();
-			loadPublikation(leftContent, "", $("#modal-searchInput > input").val(), "0");
+			sortType = "";
+			loadPublikation(leftContent, "", $("#modal-searchInput > input").val(), "0", "xml");
 		});
 		
-		$("li.next a, #previous a, #first a").click(function() {
+		$("#modalFrame li.next a, #modalFrame #previous a, #modalFrame #first a").click(function() {
 			if(!$(this).parent().hasClass("disabled")) {
-				loadPublikation(leftContent, "", $("#modal-searchInput > input").val(), $(this).attr("data"));
+				loadPublikation(leftContent, "", $("#modal-searchInput > input").val(), $(this).attr("data"), "xml");
 				$("#main_right_content").empty();
 			}
 		});
 		
-		function loadPublikation(callback, href, qry, start){
+		$(".modal-footer select").change(function() {
+			sortType = decodeURIComponent($(this).val());
+			loadPublikation(leftContent, "", "", "0", "xml");
+			$("#main_right_content").empty();
+		});
+		
+		function loadPublikation(callback, href, qry, start, dataType){
 			var url = href;
 			if(url == "") {
-				url = "servlets/solr/find?qry=" + qry + "&start=" + start + "&rows=10&XSL.Style=xml";
+				url = "servlets/solr/find?qry=" + qry + sortType + "&start=" + start + "&rows=10&XSL.Style=xml";
 			}
 			$.ajax({
 				url: webApplicationBaseURL + url,
 				type: "GET",
-				dataType: "html",
+				dataType: dataType,
 				success: function(data) {
 					callback(data);
 				},
