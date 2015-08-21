ec741b865b24fff859383f5071a9b1aca98b9a6b
Kathleen Neumann <neumann@gbv.de>
2015-08-21 15:01:52 +0000

    MIR-230 #comment do some refacturing
    git-svn-id: https://server.mycore.de/svn/maven/mir/trunk@33320 7089d063-bc14-496d-ac90-53de1872eb6c

--- mir-it/src/test/java/org/mycore/mir/it/MIRBaseITCase.java
+++ mir-it/src/test/java/org/mycore/mir/it/MIRBaseITCase.java
@@ -52,7 +52,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
@@ -184,18 +184,9 @@ public class MIRBaseITCase {
 
     public void loginAs(String user, String password) {
 
-        // Here's the explicit wait.
-        WebDriverWait wdw = new WebDriverWait(driver, 10);
-        ExpectedCondition<Boolean> condition = new ExpectedCondition<Boolean>() {
-           @Override
-           public Boolean apply(WebDriver d) {
-              WebElement result = d.findElement(By.id("loginURL"));
-              return "Anmelden".equals(result.getText());
-              // Returns true as soon as an element of id 'loginURL' is found
-              // where the element's text value is "Anmelden".
-          }
-        };
-        wdw.until(condition); // Won't get past here til timeout or element is found
+        // waits up to 10 seconds before throwing a TimeoutException or goes on if element is displayed and enabled
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("loginURL")));
 
         driver.findElement(By.id("loginURL")).click();
         assertEquals("Anmelden mit lokaler Nutzerkennung", driver.getTitle());
