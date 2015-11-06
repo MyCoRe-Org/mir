58221248a7985a0e762aa51c52662da4cbb51533
Sebastian Hofmann <hofmann.s@uni-jena.de>
2015-11-06 15:41:26 +0000

    MIR-265 #resolve Sword2 implementation for MIR
    git-svn-id: https://server.mycore.de/svn/maven/mir/trunk@33851 7089d063-bc14-496d-ac90-53de1872eb6c

---
+++ mir-module/src/main/java/org/mycore/mir/sword2/MIRSwordCollectionProvider.java
@@ -0,0 +1,77 @@
+package org.mycore.mir.sword2;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.mycore.sword.application.MCRSwordAuthHandler;
+import org.mycore.sword.application.MCRSwordCollectionProvider;
+import org.mycore.sword.application.MCRSwordDefaultAuthHandler;
+import org.mycore.sword.application.MCRSwordIngester;
+import org.mycore.sword.application.MCRSwordLifecycleConfiguration;
+import org.mycore.sword.application.MCRSwordMetadataProvider;
+import org.mycore.sword.application.MCRSwordObjectIDSupplier;
+import org.mycore.sword.application.MCRSwordSolrObjectIDSupplier;
+import org.swordapp.server.UriRegistry;
+
+/**
+ * @author Sebastian Hofmann (mcrshofm)
+ */
+public class MIRSwordCollectionProvider extends MCRSwordCollectionProvider {
+
+    private MCRSwordDefaultAuthHandler mcrSwordDefaultAuthHandler;
+    private MIRSwordMetadataProvider mirSwordMetadataProvider;
+    private MIRSwordIngester mirSwordIngester;
+    private MCRSwordSolrObjectIDSupplier mcrSwordSolrObjectIDSupplier;
+
+    public MIRSwordCollectionProvider() {
+        mcrSwordDefaultAuthHandler = new MCRSwordDefaultAuthHandler();
+        mirSwordMetadataProvider = new MIRSwordMetadataProvider();
+        mirSwordIngester = new MIRSwordIngester();
+        mcrSwordSolrObjectIDSupplier = new MCRSwordSolrObjectIDSupplier(new SolrQuery("objectType:mods"));
+    }
+
+    @Override
+    public boolean isVisible() {
+        // this collection can be seen by everybody
+        return true;
+    }
+
+    @Override
+    public List<String> getSupportedPagacking() {
+        return Arrays.asList(UriRegistry.PACKAGE_SIMPLE_ZIP);
+    }
+
+    @Override
+    public MCRSwordObjectIDSupplier getIDSupplier() {
+        return mcrSwordSolrObjectIDSupplier;
+    }
+
+    @Override
+    public MCRSwordIngester getIngester() {
+        return mirSwordIngester;
+    }
+
+    @Override
+    public MCRSwordMetadataProvider getMetadataProvider() {
+        return mirSwordMetadataProvider;
+    }
+
+    @Override
+    public MCRSwordAuthHandler getAuthHandler() {
+        return mcrSwordDefaultAuthHandler;
+    }
+
+    @Override
+    public void init(MCRSwordLifecycleConfiguration lifecycleConfiguration) {
+        super.init(lifecycleConfiguration);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+
+}
---
+++ mir-module/src/main/java/org/mycore/mir/sword2/MIRSwordMetadataProvider.java
@@ -0,0 +1,89 @@
+package org.mycore.mir.sword2;
+
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.abdera.i18n.iri.IRI;
+import org.apache.abdera.model.Entry;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.mycore.common.content.MCRBaseContent;
+import org.mycore.common.content.MCRContent;
+import org.mycore.common.content.transformer.MCRXSL2XMLTransformer;
+import org.mycore.datamodel.metadata.MCRMetadataManager;
+import org.mycore.datamodel.metadata.MCRObject;
+import org.mycore.datamodel.metadata.MCRObjectID;
+import org.mycore.sword.MCRSwordUtil;
+import org.mycore.sword.application.MCRSwordLifecycleConfiguration;
+import org.mycore.sword.application.MCRSwordMetadataProvider;
+import org.swordapp.server.DepositReceipt;
+import org.swordapp.server.SwordError;
+import org.swordapp.server.UriRegistry;
+import org.xml.sax.SAXException;
+
+public class MIRSwordMetadataProvider extends MCRSwordMetadataProvider {
+
+    private static final MCRXSL2XMLTransformer XSL_MODS_DC_TRANSFORMER = new MCRXSL2XMLTransformer("xsl/mycoreobject-mods.xsl", "xsl/mods2dc.xsl");
+    private MCRSwordLifecycleConfiguration lifecycleConfiguration;
+
+    @Override
+    public DepositReceipt provideMetadata(MCRObject object) throws SwordError {
+        final IRI iri = new IRI(MCRSwordUtil.BuildLinkUtil.getEditHref(this.lifecycleConfiguration.getCollection(), object.getId().toString()));
+        final DepositReceipt depositReceipt = MCRSwordUtil.buildDepositReceipt(iri);
+        addMetadata(object, depositReceipt);
+        final Entry we = depositReceipt.getWrappedEntry();
+        MCRSwordUtil.BuildLinkUtil.getEditMediaIRIStream(this.lifecycleConfiguration.getCollection(), object.getId().toString())
+                .forEach(we::addLink);
+        return depositReceipt;
+    }
+
+
+    @Override
+    public Entry provideListMetadata(MCRObjectID id) throws SwordError {
+        final Entry entry = super.provideListMetadata(id);
+
+        MCRObject mcrObject = MCRMetadataManager.retrieveMCRObject(id);
+        MCRSwordUtil.addDatesToEntry(entry, mcrObject);
+        return entry;
+    }
+
+    public void addMetadata(MCRObject object, DepositReceipt receipt) throws SwordError {
+        final MCRBaseContent mcrBaseContent = new MCRBaseContent(object);
+        final MCRContent mcrContent;
+        try {
+            mcrContent = XSL_MODS_DC_TRANSFORMER.transform(mcrBaseContent);
+        } catch (IOException e) {
+            throw new SwordError(UriRegistry.ERROR_BAD_REQUEST, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error while transforming mods2dc!", e);
+        }
+
+        final List<Element> elementList;
+        try {
+            elementList = mcrContent.asXML().getRootElement().getChildren();
+        } catch (JDOMException | IOException | SAXException e) {
+            throw new SwordError(UriRegistry.ERROR_BAD_REQUEST, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error getting transform result of mods to dc transformation!", e);
+        }
+
+        elementList.stream()
+                .filter(dcElement -> dcElement.getText().trim().length() > 0)
+                .forEach(dcElement -> {
+                    receipt.addDublinCore(dcElement.getName(), dcElement.getText().trim());
+                });
+
+        MCRSwordUtil.addDatesToEntry(receipt.getWrappedEntry(), object);
+    }
+
+
+    @Override
+    public void init(MCRSwordLifecycleConfiguration lifecycleConfiguration) {
+        super.init(lifecycleConfiguration);
+        this.lifecycleConfiguration = lifecycleConfiguration;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
