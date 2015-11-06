142302ef30f7053d186d8e7f607852c21430196b
Sebastian Hofmann <hofmann.s@uni-jena.de>
2015-11-06 15:50:46 +0000

    MIR-265 #comment use correct ids
    git-svn-id: https://server.mycore.de/svn/maven/mir/trunk@33855 7089d063-bc14-496d-ac90-53de1872eb6c

--- mir-module/src/main/java/org/mycore/mir/sword2/MIRSwordIngester.java
+++ mir-module/src/main/java/org/mycore/mir/sword2/MIRSwordIngester.java
@@ -13,6 +13,7 @@ import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.Namespace;
+import org.mycore.common.config.MCRConfiguration;
 import org.mycore.common.content.MCRContent;
 import org.mycore.common.content.MCRJDOMContent;
 import org.mycore.common.content.transformer.MCRXSL2XMLTransformer;
@@ -40,7 +41,7 @@ public class MIRSwordIngester implements MCRSwordIngester {
 
     @Override
     public MCRObjectID ingestMetadata(Deposit entry) throws SwordError, SwordServerException {
-        final MCRObjectID newObjectId = MCRObjectID.getNextFreeIdByType("mods");
+        final MCRObjectID newObjectId = MCRObjectID.getNextFreeId(MCRConfiguration.instance().getString("MIR.projectid.default") + "_mods");
         final Map<String, List<String>> dublinCoreMetadata = entry.getSwordEntry().getDublinCore();
 
         Document dcDocument = buildDCDocument(dublinCoreMetadata);
