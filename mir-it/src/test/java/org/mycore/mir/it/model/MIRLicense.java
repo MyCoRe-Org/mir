16e0ac17a06bfea1ee3a070f1479be8ca97e6e5e
Michel Büchner <michel.buechner@uni-jena.de>
2016-06-14 09:17:49 +0000

    MCR-1302 #comment fixed typo
    git-svn-id: https://server.mycore.de/svn/maven/mir/trunk@35606 7089d063-bc14-496d-ac90-53de1872eb6c

--- mir-it/src/test/java/org/mycore/mir/it/model/MIRLicense.java
+++ mir-it/src/test/java/org/mycore/mir/it/model/MIRLicense.java
@@ -2,7 +2,7 @@ package org.mycore.mir.it.model;
 
 public enum MIRLicense {
     rights_reserved("rights_reserved"),
-    cc_40("cc_by_4.0");
+    cc_by_40("cc_by_4.0");
 
 
     private String value;
