749669299a8951cd17472ec65bb98214e2c142a7
Sebastian Hofmann <hofmann.s@uni-jena.de>
2018-02-20 14:25:55 +0100

    MCR-1748 Only insert bootstrap Javascript when bootstrap is not already embedded

--- mir-layout/src/main/java/org/mycore/mir/viewer/MIRViewerConfigurationStrategy.java
+++ mir-layout/src/main/java/org/mycore/mir/viewer/MIRViewerConfigurationStrategy.java
@@ -32,9 +32,6 @@ public class MIRViewerConfigurationStrategy extends MCRViewerDefaultConfiguratio
                 mcrViewerConfiguration.addCSS(customLayoutURL);
             }
 
-            // Default JS
-            mcrViewerConfiguration
-                .addScript(MCRFrontendUtil.getBaseURL() + "assets/bootstrap/js/bootstrap.min.js");
             String customJS = params.getParameter("MIR.CustomLayout.JS", "");
             if (customJS.length() > 0) {
                 mcrViewerConfiguration.addScript(String.format(Locale.ROOT, "%sjs/%s", baseURL, customJS));
@@ -43,6 +40,10 @@ public class MIRViewerConfigurationStrategy extends MCRViewerDefaultConfiguratio
             if(request.getParameter("embedded")!=null){
                 mcrViewerConfiguration.setProperty("permalink.updateHistory", false);
                 mcrViewerConfiguration.setProperty("chapter.showOnStart", false);
+            } else {
+                // Default JS
+                mcrViewerConfiguration
+                    .addScript(MCRFrontendUtil.getBaseURL() + "assets/bootstrap/js/bootstrap.min.js");
             }
         }
 
