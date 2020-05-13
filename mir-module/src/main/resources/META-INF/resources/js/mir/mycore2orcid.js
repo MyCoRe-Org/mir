a8d31b552061d1bb7f1ba895c28a32e2ceff213c
Sebastian Hofmann <hofmann.s@uni-jena.de>
2020-05-13 17:17:28 +0200

    MCR-902 use message properties in orcid javascript

--- mir-module/src/main/resources/META-INF/resources/js/mir/mycore2orcid.js
+++ mir-module/src/main/resources/META-INF/resources/js/mir/mycore2orcid.js
@@ -1,73 +1,99 @@
-var orcidStatusURL  = webApplicationBaseURL + "rsc/orcid/status/";
-var orcidPublishURL = webApplicationBaseURL + "rsc/orcid/publish/";
+(function(){
+	const orcidStatusURL  = window["webApplicationBaseURL"] + "rsc/orcid/status/";
+	const orcidPublishURL = window["webApplicationBaseURL"] + "rsc/orcid/publish/";
+	const translateURL = window["webApplicationBaseURL"] + "rsc/locale/translate/";
 
-var orcidIcon = "<img alt='ORCID iD' src='" + webApplicationBaseURL + "images/orcid_icon.svg' class='orcid-icon' />";
+	const orcidIcon = "<img alt='ORCID iD' src='" + window["webApplicationBaseURL"] + "images/orcid_icon.svg'" +
+		" class='orcid-icon' />";
 
-var orcidTextStatusIsInProfileTrue  = "Diese Publikation ist in Ihrem ORCID-Profil vorhanden";
-var orcidTextStatusIsInProfileFalse = "Diese Publikation ist NICHT in Ihrem ORCID-Profil vorhanden";
+	let orcidTextStatusIsInProfileTrue  = "orcid.publication.inProfile.true";
+	let orcidTextStatusIsInProfileFalse = "orcid.publication.inProfile.false";
 
-var orcidTextPublishUpdate = "Im ORCID Profil aktualisieren";
-var orcidTextPublishCreate = "Ins ORCID Profil übertragen";
-var orcidTextPublishConfirm = "Die Publikation wurde in Ihr ORCID Profil übertragen!";
+	let orcidTextPublishUpdate = "orcid.publication.action.update";
+	let orcidTextPublishCreate = "orcid.publication.action.create ";
+	let orcidTextPublishConfirm = "orcid.publication.action.confirmation";
+	const keyPrefix = "orcid.publication*";
 
-jQuery(document).ready(function() {
-	jQuery('div.orcid-status').each(function() {
-		getORCIDPublicationStatus(this);
-	});
-	jQuery('div.orcid-publish').each(function() {
-		showORCIDPublishButton(this);
-	});
-});
+	let docReady=false;
+	let langReady=false;
 
-function getORCIDPublicationStatus(div) {
-	var id = jQuery(div).data('id');
-	jQuery.get(orcidStatusURL + id, function(status) {
-		console.log(status);
-		setORCIDPublicationStatus(div, status);
-	});
-}
-
-function setORCIDPublicationStatus(div, status) {
-	jQuery(div).empty();
-	if (status.user.isORCIDUser && status.isUsersPublication) {
-		var html = "<span class='orcid-info' title='"
-			+ (status.isInORCIDProfile ? orcidTextStatusIsInProfileTrue : orcidTextStatusIsInProfileFalse)	+ "'>";
-		html += orcidIcon;
-		html += "<span class='far fa-thumbs-" + (status.isInORCIDProfile ? "up" : "down") 
-		        + " orcid-in-profile-" + status.isInORCIDProfile + "' aria-hidden='true' />";
-		html += "</span>";
-		jQuery(div).html(html);
+	function tryInit(){
+		if (langReady && docReady) {
+			jQuery('div.orcid-status').each(function() {
+				getORCIDPublicationStatus(this);
+			});
+			jQuery('div.orcid-publish').each(function() {
+				showORCIDPublishButton(this);
+			});
+		}
 	}
-}
 
-function showORCIDPublishButton(div) {
-	var id = jQuery(div).data('id');
-	jQuery.get(orcidStatusURL + id, function(status) {
-		console.log(status);
-		updateORCIDPublishButton(div, status);
+	jQuery.ajax(translateURL + window["currentLang"] + "/" + keyPrefix).done(function (data) {
+		orcidTextStatusIsInProfileTrue = data[orcidTextStatusIsInProfileTrue];
+		orcidTextStatusIsInProfileFalse = data[orcidTextStatusIsInProfileFalse];
+		orcidTextPublishUpdate = data[orcidTextPublishUpdate];
+		orcidTextPublishCreate = data[orcidTextPublishCreate];
+		orcidTextPublishConfirm = data[orcidTextPublishConfirm];
+	}).always(function () {
+		langReady = true;
+		tryInit();
 	});
-}
 
-function updateORCIDPublishButton(div, status) {
-	var id = jQuery(div).data('id');
-	jQuery(div).empty();
-	if (status.user.isORCIDUser && status.user.weAreTrustedParty && status.isUsersPublication) {
-		var html = "<button class='orcid-button'>"
-				+ (status.isInORCIDProfile ? orcidTextPublishUpdate	: orcidTextPublishCreate) + "</button>";
-		jQuery(div).html(html);
-		jQuery(div).find('.orcid-button').click( function() {
-					div = this;
-					jQuery.get(orcidPublishURL + id, function(newStatus) {
-						alert(orcidTextPublishConfirm);
-						jQuery("div.orcid-status[data-id='" + id + "']").each(
-								function() {
-									setORCIDPublicationStatus(this, newStatus);
-								});
-						jQuery("div.orcid-publish[data-id='" + id + "']").each(
-								function() {
-									updateORCIDPublishButton(this, newStatus);
-								});
+	jQuery(document).ready(function() {
+		docReady=true;
+		tryInit();
+	});
+
+	function getORCIDPublicationStatus(div) {
+		var id = jQuery(div).data('id');
+		jQuery.get(orcidStatusURL + id, function(status) {
+			console.log(status);
+			setORCIDPublicationStatus(div, status);
+		});
+	}
+
+	function setORCIDPublicationStatus(div, status) {
+		jQuery(div).empty();
+		if (status.user.isORCIDUser && status.isUsersPublication) {
+			var html = "<span class='orcid-info' title='"
+				+ (status.isInORCIDProfile ? orcidTextStatusIsInProfileTrue : orcidTextStatusIsInProfileFalse)	+ "'>";
+			html += orcidIcon;
+			html += "<span class='far fa-thumbs-" + (status.isInORCIDProfile ? "up" : "down")
+					+ " orcid-in-profile-" + status.isInORCIDProfile + "' aria-hidden='true' />";
+			html += "</span>";
+			jQuery(div).html(html);
+		}
+	}
+
+	function showORCIDPublishButton(div) {
+		var id = jQuery(div).data('id');
+		jQuery.get(orcidStatusURL + id, function(status) {
+			console.log(status);
+			updateORCIDPublishButton(div, status);
+		});
+	}
+
+	function updateORCIDPublishButton(div, status) {
+		var id = jQuery(div).data('id');
+		jQuery(div).empty();
+		if (status.user.isORCIDUser && status.user.weAreTrustedParty && status.isUsersPublication) {
+			var html = "<button class='orcid-button'>"
+					+ (status.isInORCIDProfile ? orcidTextPublishUpdate	: orcidTextPublishCreate) + "</button>";
+			jQuery(div).html(html);
+			jQuery(div).find('.orcid-button').click( function() {
+						div = this;
+						jQuery.get(orcidPublishURL + id, function(newStatus) {
+							alert(orcidTextPublishConfirm);
+							jQuery("div.orcid-status[data-id='" + id + "']").each(
+									function() {
+										setORCIDPublicationStatus(this, newStatus);
+									});
+							jQuery("div.orcid-publish[data-id='" + id + "']").each(
+									function() {
+										updateORCIDPublishButton(this, newStatus);
+									});
+						});
 					});
-				});
+		}
 	}
-}
+}());
