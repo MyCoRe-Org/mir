5f1ad86a8184e7c7d2a99b90e56042132e5815e6
Sebastian Hofmann <hofmann.s@uni-jena.de>
2020-07-21 11:44:46 +0200

    MIR-924 added whitelisting for HTML-Elements

--- mir-module/src/main/java/org/mycore/mir/editor/MIREditorUtils.java
+++ mir-module/src/main/java/org/mycore/mir/editor/MIREditorUtils.java
@@ -1,8 +1,13 @@
 package org.mycore.mir.editor;
 
+import java.util.stream.Stream;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Entities;
+import org.jsoup.safety.Cleaner;
+import org.jsoup.safety.Whitelist;
+import org.mycore.common.config.MCRConfiguration2;
 
 public class MIREditorUtils {
 
@@ -12,12 +17,29 @@ public class MIREditorUtils {
     }
 
     public static String getXHTMlSnippedString(String text) {
-        final Document document = Jsoup.parse(text);
+        Document document = Jsoup.parse(text);
         document.outputSettings().syntax(Document.OutputSettings.Syntax.xml);
         document.outputSettings().escapeMode(Entities.EscapeMode.xhtml);
-        final String bodyContent = document.body().html();
 
-        return bodyContent;
+        final Whitelist elementWhitelist = getWhiteList();
+        document = getCleanDocument(document, elementWhitelist);
+        return document.body().html();
+    }
+
+    private static Document getCleanDocument(Document document, Whitelist elementWhitelist) {
+        return new Cleaner(elementWhitelist).clean(document);
+    }
+
+    private static Whitelist getWhiteList() {
+        final Whitelist elementWhitelist = Whitelist.none();
+        elementWhitelist.addTags("html", "head", "body");
+
+        final Stream<String> allowedElements = MCRConfiguration2.getString("MIR.Editor.HTML.Elements")
+                .map(s -> Stream.of(s.split(" ")))
+                .orElseThrow(() -> MCRConfiguration2.createConfigurationException("MIR.Editor.HTML.Elements"));
+
+        allowedElements.forEach(elementWhitelist::addTags);
+        return elementWhitelist;
     }
 
 }
--- mir-module/src/main/resources/META-INF/resources/js/ckeditor-config.js
+++ mir-module/src/main/resources/META-INF/resources/js/ckeditor-config.js
@@ -5,11 +5,12 @@ CKEDITOR.editorConfig = function( config ) {
         { name: 'clipboard', items: ['Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
         { name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'CopyFormatting', 'RemoveFormat' ] },
         { name: 'insert', items: [ 'Table', 'HorizontalRule', 'SpecialChar' ] },
-        { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
         { name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyBlock', '-' ] },
         { name: 'styles', items: [ 'Styles', 'Format', 'FontSize' ] },
         { name: 'about', items: [ 'About' ] }
     ];
+
+    config.allowedContent = window["MIR.Editor.HTML.Elements"];
 
 };
 
