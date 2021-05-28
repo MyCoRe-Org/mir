ebb4945ec56f9fcc09339ed9a49ccf54106d8578
Kathleen Neumann <neumann@gbv.de>
2021-05-28 16:32:41 +0200

    MIR-1021 removed also obsolete js

--- mir-module/src/main/resources/META-INF/resources/js/mir/xeditor-form.js
+++ mir-module/src/main/resources/META-INF/resources/js/mir/xeditor-form.js
@@ -158,12 +158,6 @@ $(document).ready(function() {
         });
     }
 
-    if ($("input.managedIdentifier").length > 0 && $("select.identifierSelect").length > 0) {
-        $("input.managedIdentifier").each(function() {
-            $("select.identifierSelect").find("option[value='" + $(this).attr("data-type") + "']").remove();
-        });
-    }
-
     function setLabelForClassification(parent) {
         $.ajax({
             url: webApplicationBaseURL + 'servlets/solr/select',
