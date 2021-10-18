0a30b5dd426957b037142c131fd2f797ad3f7e02
Sebastian Hofmann <sebastian.hofmann@gbv.de>
2021-10-18 13:32:34 +0200

    MIR-1051 Invalidate permission cache after changing status

--- mir-module/src/main/java/org/mycore/mir/MIRStateServlet.java
+++ mir-module/src/main/java/org/mycore/mir/MIRStateServlet.java
@@ -1,7 +1,11 @@
 package org.mycore.mir;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,6 +15,8 @@ import org.mycore.datamodel.classifications2.MCRCategoryDAO;
 import org.mycore.datamodel.classifications2.MCRCategoryDAOFactory;
 import org.mycore.datamodel.classifications2.MCRCategoryID;
 import org.mycore.datamodel.classifications2.MCRLabel;
+import org.mycore.datamodel.metadata.MCRMetaLink;
+import org.mycore.datamodel.metadata.MCRMetaLinkID;
 import org.mycore.datamodel.metadata.MCRMetadataManager;
 import org.mycore.datamodel.metadata.MCRObject;
 import org.mycore.datamodel.metadata.MCRObjectID;
@@ -21,6 +27,10 @@ import org.mycore.frontend.servlets.MCRServletJob;
 public class MIRStateServlet extends MCRServlet {
 
     protected static final String X_NEXT_LANGUAGE = "x-next";
+
+    private static final List<String> IMPORTANT_PERMISSIONS = Stream.of(MCRAccessManager.PERMISSION_READ,
+        MCRAccessManager.PERMISSION_WRITE, MCRAccessManager.PERMISSION_DELETE, MCRAccessManager.PERMISSION_VIEW)
+        .collect(Collectors.toList());
 
     @Override
     protected void doGetPost(MCRServletJob job) throws Exception {
@@ -73,6 +83,20 @@ public class MIRStateServlet extends MCRServlet {
 
         object.getService().setState(newStateCategory);
         MCRMetadataManager.update(object);
+
+        // remove all cached permission of derivates and children
+        List<String> permissionObjectIds = new ArrayList<>();
+        permissionObjectIds.add(object.getId().toString());
+
+        object.getStructure().getDerivates().stream().map(MCRMetaLink::getXLinkHref).forEach(permissionObjectIds::add);
+        object.getStructure().getChildren().stream().map(MCRMetaLink::getXLinkHref).forEach(permissionObjectIds::add);
+
+        for (String permissionObjectId : permissionObjectIds) {
+            for (String permission : IMPORTANT_PERMISSIONS) {
+                MCRAccessManager.invalidPermissionCache(permissionObjectId, permission);
+            }
+        }
+        
         job.getResponse().sendRedirect(MCRFrontendUtil.getBaseURL(job.getRequest()) + "receive/" + objectID.toString());
     }
 }
