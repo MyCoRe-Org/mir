9abedd4b553ac7f9cfbe8640f120913b3802a79d
Sebastian Hofmann <sebastian.hofmann@gbv.de>
2022-09-21 17:40:01 +0200

    MCR-1116 added rules for new fact based acl system

---
+++ mir-module/src/test/java/org/mycore/mir/acl/MIRCreateACLTest.java
@@ -0,0 +1,63 @@
+/*
+ *  This file is part of ***  M y C o R e  ***
+ *  See http://www.mycore.de/ for details.
+ *
+ *  MyCoRe is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MyCoRe is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MyCoRe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.mycore.mir.acl;
+
+import org.junit.Test;
+
+public class MIRCreateACLTest extends MIRACLTest {
+
+
+    @Test
+    public void testCreateGuest() {
+        testPermission("guest", "create-mods", false, "");
+
+        testPermission("guest", "create-derivate", false, "");
+    }
+
+    @Test
+    public void testCreateAdmin(){
+        enableDebug();
+        testPermission("admin1", "create-mods", true, MIRTestConstants.MIR_ROLES_ADMIN);
+        disableDebug();
+
+        testPermission("admin1", "create-derivate", true,  MIRTestConstants.MIR_ROLES_ADMIN);
+    }
+
+    @Test
+    public void testCreateEditor(){
+        testPermission("editor1", "create-mods", true,  MIRTestConstants.MIR_ROLES_EDITOR);
+
+        testPermission("editor1", "create-derivate", true,  MIRTestConstants.MIR_ROLES_EDITOR);
+    }
+
+    @Test
+    public void testCreateReader(){
+        testPermission("reader1", "create-mods", false,  MIRTestConstants.MIR_ROLES_READER);
+
+        testPermission("reader1", "create-derivate", false,  MIRTestConstants.MIR_ROLES_READER);
+    }
+
+    @Test
+    public void testCreateSubmitter(){
+        testPermission("submitter1", "create-mods", true,  MIRTestConstants.MIR_ROLES_SUBMITTER);
+
+        testPermission("submitter1", "create-derivate", true,  MIRTestConstants.MIR_ROLES_SUBMITTER);
+    }
+
+}
---
+++ mir-module/src/test/java/org/mycore/mir/acl/MIREditorACLTest.java
@@ -0,0 +1,65 @@
+/*
+ *  This file is part of ***  M y C o R e  ***
+ *  See http://www.mycore.de/ for details.
+ *
+ *  MyCoRe is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MyCoRe is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MyCoRe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.mycore.mir.acl;
+
+import org.junit.Test;
+
+/**
+ * Checks if a user can open a specific editor
+ */
+public class MIREditorACLTest extends MIRACLTest {
+
+    @Test
+    public void testEditorGuest() {
+        checkWebpage( "/editor/editor-dynamic.xed", false, "");
+        checkWebpage( "/editor/editor-derivate.xed", false, "");
+        checkWebpage( "/editor/editor-admins.xed", false, "");
+    }
+
+    @Test
+    public void testEditorAdmin(){
+        checkWebpage( "/editor/editor-dynamic.xed", true, MIRTestConstants.MIR_ROLES_ADMIN);
+        checkWebpage( "/editor/editor-derivate.xed", true, MIRTestConstants.MIR_ROLES_ADMIN);
+        checkWebpage( "/editor/editor-admins.xed", true, MIRTestConstants.MIR_ROLES_ADMIN);
+    }
+
+    @Test
+    public void testEditorEditor(){
+        checkWebpage("/editor/editor-dynamic.xed", true, MIRTestConstants.MIR_ROLES_EDITOR);
+        checkWebpage( "/editor/editor-derivate.xed", true, MIRTestConstants.MIR_ROLES_EDITOR);
+        checkWebpage( "/editor/editor-admins.xed", true, MIRTestConstants.MIR_ROLES_EDITOR);
+
+    }
+
+    @Test
+    public void testEditorReader(){
+        checkWebpage( "/editor/editor-dynamic.xed", false, MIRTestConstants.MIR_ROLES_READER);
+        checkWebpage( "/editor/editor-derivate.xed", false, MIRTestConstants.MIR_ROLES_READER);
+        checkWebpage( "/editor/editor-admins.xed", false, MIRTestConstants.MIR_ROLES_READER);
+    }
+
+    @Test
+    public void testEditorSubmitter(){
+        checkWebpage( "/editor/editor-dynamic.xed", true, MIRTestConstants.MIR_ROLES_SUBMITTER);
+        checkWebpage( "/editor/editor-derivate.xed", true, MIRTestConstants.MIR_ROLES_SUBMITTER);
+        checkWebpage( "/editor/editor-admins.xed", false, MIRTestConstants.MIR_ROLES_SUBMITTER);
+    }
+
+
+}
