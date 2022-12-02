da511e740910aceabf4bc418af83dfe21c475281
Tilo Neumann <tilo.neumann@gbv.de>
2022-12-02 14:16:35 +0100

    MIR-1187 adds button logic to base script and styles to common

--- mir-module/src/main/resources/META-INF/resources/js/mir/base.js
+++ mir-module/src/main/resources/META-INF/resources/js/mir/base.js
@@ -626,4 +626,33 @@
     container : "body"
   });
 
+  /* =================
+  /   go back to top
+  /  ================= */
+  // create html code for button
+  var back_to_top_button = ['<button class="btn back-to-top"><i class="fas fa-chevron-circle-up"></i></button>'].join("");
+  // add button to page
+  $("#page").append(back_to_top_button);
+  // hide button
+  $(".back-to-top").hide();
+  // on scroll
+  $(function () {
+    $(window).scroll(function () {
+      // if scrolled down
+      if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn();
+      } else {
+        $('.back-to-top').fadeOut();
+      }
+    });
+  });
+  // click button
+  $('.back-to-top').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
+  /* end: go back to top */
+
 })(jQuery);
