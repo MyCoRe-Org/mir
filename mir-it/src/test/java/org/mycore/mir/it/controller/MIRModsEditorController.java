8cf98647bae47951039c72c4b6973100e6ba36c6
Sebastian Hofmann <7668803+sebhofmann@users.noreply.github.com>
2022-12-05 11:26:21 +0100

    MIR-1188 Fix the mir build (#828)
    * MIR-1188 Fix the mir build
    
    - Use new mycore-classifications module to provide the default
      classifications
    - Fix abstract tests

--- mir-it/src/test/java/org/mycore/mir/it/controller/MIRModsEditorController.java
+++ mir-it/src/test/java/org/mycore/mir/it/controller/MIRModsEditorController.java
@@ -6,6 +6,7 @@ import java.util.stream.IntStream;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.mycore.common.MCRException;
 import org.mycore.common.selenium.drivers.MCRWebdriverWrapper;
 import org.mycore.mir.it.model.MIRAbstract;
 import org.mycore.mir.it.model.MIRAccess;
