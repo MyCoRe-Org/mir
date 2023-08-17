bbc44e5e410c50ef611d17de2c5cd2618553dfb5
Torsten Krause <krause.torsten@ub.uni-kiel.de>
2023-08-17 17:49:48 +0200

    MIR-1236 update remaining references to javax.servlet

--- mir-wizard/src/main/java/org/mycore/mir/wizard/MIRWizardRequestFilter.java
+++ mir-wizard/src/main/java/org/mycore/mir/wizard/MIRWizardRequestFilter.java
@@ -66,7 +66,7 @@ public class MIRWizardRequestFilter implements Filter {
     }
 
     /* (non-Javadoc)
-     * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+     * @see jakarta.servlet.Filter#init(jakarta.servlet.FilterConfig)
      */
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -82,7 +82,7 @@ public class MIRWizardRequestFilter implements Filter {
     }
 
     /* (non-Javadoc)
-     * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
+     * @see jakarta.servlet.Filter#doFilter(jakarta.servlet.ServletRequest, jakarta.servlet.ServletResponse, jakarta.servlet.FilterChain)
      */
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
@@ -107,7 +107,7 @@ public class MIRWizardRequestFilter implements Filter {
     }
 
     /* (non-Javadoc)
-     * @see javax.servlet.Filter#destroy()
+     * @see jakarta.servlet.Filter#destroy()
      */
     @Override
     public void destroy() {
