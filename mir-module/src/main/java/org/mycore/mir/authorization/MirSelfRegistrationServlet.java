5a4dd52f48db13983ec58d9e429ead4c5b76a82f
Torsten Krause <krause.torsten@ub.uni-kiel.de>
2024-07-04 13:00:25 +0200

    MIR-3137 modernize password hashing and hash verification

--- mir-module/src/main/java/org/mycore/mir/authorization/MirSelfRegistrationServlet.java
+++ mir-module/src/main/java/org/mycore/mir/authorization/MirSelfRegistrationServlet.java
@@ -39,7 +39,6 @@ import org.mycore.frontend.MCRFrontendUtil;
 import org.mycore.frontend.servlets.MCRServlet;
 import org.mycore.frontend.servlets.MCRServletJob;
 import org.mycore.services.i18n.MCRTranslation;
-import org.mycore.user2.MCRPasswordHashType;
 import org.mycore.user2.MCRUser;
 import org.mycore.user2.MCRUserManager;
 import org.mycore.user2.utils.MCRUserTransformer;
@@ -161,8 +160,7 @@ public class MirSelfRegistrationServlet extends MCRServlet {
             DateTimeFormatter formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME.withZone(ZoneOffset.UTC);
             user.setUserAttribute("registeredAt", registeredAt.format(formatter));
 
-            user.setHashType(MCRPasswordHashType.md5);
-            user.setPassword(MCRUtils.asMD5String(1, null, doc.getRootElement().getChildText("password")));
+            MCRUserManager.setUserPassword(user, doc.getRootElement().getChildText("password"));
 
             MCRUserManager.createUser(user);
 
