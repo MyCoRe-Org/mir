70c9b4e38125b16938a46ce4921a8b557f687bd6
Torsten Krause <krause.torsten@ub.uni-kiel.de>
2024-09-04 16:52:33 +0200

    MIR-1351 consider case of a fresh repository w/o existing static data

--- mir-module/src/main/java/org/mycore/mir/migration/MIRMigrateStaticHistoryContentJobAction.java
+++ mir-module/src/main/java/org/mycore/mir/migration/MIRMigrateStaticHistoryContentJobAction.java
@@ -1,17 +1,17 @@
 package org.mycore.mir.migration;
 
+import static org.mycore.mir.migration.MIRMigrateStaticHistoryContent.STATIC_HISTORY_PATH;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jdom2.Document;
 import org.jdom2.input.SAXBuilder;
-import org.mycore.common.config.MCRConfiguration2;
 import org.mycore.datamodel.metadata.MCRMetadataManager;
 import org.mycore.datamodel.metadata.MCRObjectID;
 import org.mycore.services.queuedjob.MCRJob;
 import org.mycore.services.queuedjob.MCRJobAction;
 import org.mycore.services.queuedjob.staticcontent.MCRJobStaticContentGenerator;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.FileVisitResult;
@@ -21,31 +21,27 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.concurrent.ExecutionException;
 
+
 /**
  * Class converts all static history content on start up automatically.
  *
  * @author shermann (Silvio Hermann)
  * */
 public class MIRMigrateStaticHistoryContentJobAction extends MCRJobAction {
-    private Path staticHistoryPath;
-    private Logger logger;
+
+    private static final Logger LOGGER = LogManager.getLogger();
 
     public MIRMigrateStaticHistoryContentJobAction(MCRJob job) {
         super(job);
-        logger = LogManager.getLogger(MIRMigrateStaticHistoryContentJobAction.class);
-
-        staticHistoryPath = Path.of(
-            MCRConfiguration2.getStringOrThrow("MCR.Object.Static.Content.Default.Path") + File.separator
-                + "mir-history");
     }
 
     @Override
     public void execute() throws ExecutionException {
         try {
             SAXBuilder builder = new SAXBuilder();
-            Files.walkFileTree(staticHistoryPath, new SimpleFileVisitor<Path>() {
+            Files.walkFileTree(STATIC_HISTORY_PATH, new SimpleFileVisitor<>() {
                 @Override
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)  {
                     String filename = file.getFileName().toString();
                     String id = filename.substring(0, filename.lastIndexOf('.'));
 
@@ -56,19 +52,19 @@ public class MIRMigrateStaticHistoryContentJobAction extends MCRJobAction {
                     try (InputStream is = Files.newInputStream(file)) {
                         Document history = builder.build(is);
                         if ("table".equals(history.getRootElement().getName())) {
-                            logger.info("Migrating static history for object {}", id);
+                            LOGGER.info("Migrating static history for object {}", id);
                             MCRJobStaticContentGenerator generator = new MCRJobStaticContentGenerator(
                                 "mir-history");
                             generator.generate(MCRMetadataManager.retrieveMCRObject(MCRObjectID.getInstance(id)));
                         }
                     } catch (Exception e) {
-                        logger.error("Could not migrate static mcr-history for file {}", file.getFileName(), e);
+                        LOGGER.error("Could not migrate static mcr-history for file {}", file.getFileName(), e);
                     }
                     return FileVisitResult.CONTINUE;
                 }
             });
         } catch (IOException e) {
-            logger.error("Error occurred during migration of static history content", e);
+            LOGGER.error("Error occurred during migration of static history content", e);
         }
     }
 
