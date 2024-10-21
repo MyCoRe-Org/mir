8035958d0ec909447a91de785c290ba50814323e
Silvio Hermann <s.hermann@uni-jena.de>
2024-10-21 12:11:00 +0200

    MIR-1361 Enable search in ROR registry

--- mir-module/src/main/resources/META-INF/resources/js/mir/ror-search.js
+++ mir-module/src/main/resources/META-INF/resources/js/mir/ror-search.js
@@ -1,24 +1,8 @@
-/*
-$(document).ready(function () {
-
-    const handler = async function handleEvent(event) {
-        console.log(event);
-        const a = await fetch("https://api.ror.org/organizations")
-            .then(r => r.json())
-            .then(json => console.log(json));
-    }
-
-    for (const input of document.querySelectorAll('[id="mir-ror-input"]')) {
-        console.log(input.value);
-        input.addEventListener('input', handler);
-    }
-});*/
-
 const RORSearch = {
-    maxLength: 60,
+    baseURL: "https://api.ror.org/",
 
     search: async function (text) {
-        const q = await fetch("https://api.ror.org/organizations?query=" + encodeURIComponent(text));
+        const q = await fetch(RORSearch.baseURL + "organizations?query=" + encodeURIComponent(text));
         let json = await q.json();
 
         let rorIdentifiers = [];
@@ -27,34 +11,88 @@ const RORSearch = {
             let label = rorIdentifier.name;
             let item = {};
 
-            item.id = rorIdentifier.id;
-            item.label = (label.length > RORSearch.maxLength ? label.substring(0, RORSearch.maxLength).trim() + "…" : label) + " [" + rorIdentifier.name + "]";
+            item.id = rorIdentifier.id.substring(rorIdentifier.id.lastIndexOf("/") + 1);
+            item.label = label + " (" + item.id + ")";
             rorIdentifiers.push(item);
+        });
+
+        rorIdentifiers.sort((a, b) => {
+            if (a.label < b.label) {
+                return -1;
+            }
+            if (a.label > b.label) {
+                return 1;
+            }
+            return 0;
         });
         return rorIdentifiers;
     },
 
-    onRORSelected: function (event, ui) {
-        return false;
+    /**
+     * Find the option (if any) matching the given label.
+     *
+     * @param dataListId the id of the datalist
+     * @param label the label to find in the given datalist
+     *
+     * @return the option element with the givel label or null
+     * */
+    getOption: function (dataListId, label) {
+        let dataList = document.getElementById(dataListId);
+
+        for (const option of dataList.childNodes) {
+            if (option.value === label) {
+                return option;
+            }
+        }
+
+        return null;
     },
 
+    /**
+     * Searches ROR for the given text at https://api.ror.org/ .
+     * */
     onInput: async function (event) {
-        let input = $(event.target);
-        let result = await RORSearch.search(event.target.value);
+        let input = event.target;
+        let dataListId = input.getAttribute("list");
 
-        /*
-        input.autocomplete({
-            delay: 400,
-            minLength: 3,
-            source: await RORSearch.search(event.target.value),
-            select: RORSearch.onRORSelected
-        });
-         */
+        let option = RORSearch.getOption(dataListId, input.value);
+
+        // User selects value from list
+        if (option != null) {
+            input.value = option.getAttribute("data-ror");
+            event.preventDefault();
+            return;
+        }
+
+        // Selected value is not in list, actually execute search at ROR
+        let result = await RORSearch.search(input.value);
+        let dataList = document.getElementById(dataListId);
+        dataList.textContent = "";
+
+        // Update datalist with results from ROR
+        for (const suggestion of result) {
+            let option = document.createElement("option");
+            option.setAttribute("value", suggestion.label);
+            option.setAttribute("data-ror", suggestion.id);
+            dataList.appendChild(option);
+        }
     }
 };
 
+/**
+ * Adds event handler to every ror input element.
+ * */
 document.addEventListener('DOMContentLoaded', function () {
     for (const input of document.querySelectorAll('[id="mir-ror-input"]')) {
+        // create datalist element with random uuid as element id
+        let dataList = document.createElement("datalist");
+        dataList.id = self.crypto.randomUUID();
+
+        // set the previously created datalist as list to the ror input element
+        input.setAttribute("list", dataList.id);
+        input.parentElement.append(dataList);
+
+        // add the actual event handler
         input.addEventListener('input', RORSearch.onInput);
     }
 });
