45642021383baaa2b140576665b3f856893777c3
Mewel <Mewel@users.noreply.github.com>
2025-03-31 10:46:59 +0200

    MIR-1417 replace ckeditor with tinymce (#1100)
    * replace ckeditor with tinymce
    
    * MIR-1417 fix selenium test case
    
    ---------
    
    Co-authored-by: matthias <matthias@ulbp3810.ulb.uni-jena.de>

---
+++ mir-module/src/main/resources/META-INF/resources/js/tinymce-config.js
@@ -0,0 +1,20 @@
+
+tinymce.init({
+    selector: "textarea.tinymce",
+    license_key: "gpl",
+    promotion: false,
+    language: currentLang,
+    plugins: [
+        "advlist", "anchor", "autolink", "code", "fullscreen", "help",
+        "lists", "preview",
+        "searchreplace", "table", "visualblocks", "wordcount"
+    ],
+    menubar: false,
+    toolbar: "undo redo | styles | bold italic underline | remove | code | indent outdent |" +
+        " alignleft aligncenter alignright alignjustify | bullist numlist | table | hr",
+    toolbar_mode: "wrap",
+    entity_encoding: "raw",
+    valid_elements: JSON.parse(window["MIR.WebConfig.Editor.TinyMCE.HTML.Elements"] || "{}"),
+    convert_urls: false,
+    verify_html: false
+});
