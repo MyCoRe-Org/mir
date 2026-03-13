MyCoRe-Org/mir

commit 06ff8fd93461c59ef9c142d0e3ae4d287df6a17e

MIR-1540 fixed more review issues and added more tests

mir-module/src/main/resources/xslt/editor/xeditor2mods.xsl
@@ -242,6 +242,21 @@
     </mods:typeOfResource>
   </xsl:template>
 
+  <xsl:template match="*[@mcr:categId]">
+    <xsl:variable name="classid" select="mirmapper:classid(@mcr:categId)" />
+    <xsl:variable name="categid" select="mirmapper:categid(@mcr:categId)" />
+    <xsl:variable name="classURI" select="mirmapper:class-uri($classid)" />
+    <xsl:copy>
+      <xsl:copy-of select="@*[not(namespace-uri() = 'http://www.mycore.org/' and local-name() = 'categId')
+                               and not($classURI and local-name() = ('authorityURI', 'valueURI'))]" />
+      <xsl:if test="$classURI">
+        <xsl:attribute name="authorityURI" select="$classURI" />
+        <xsl:attribute name="valueURI" select="concat($classURI, '#', $categid)" />
+      </xsl:if>
+      <xsl:apply-templates select="node()" />
+    </xsl:copy>
+  </xsl:template>
+
   <xsl:template match="mods:openAireID">
     <mods:identifier type="open-aire">
       <xsl:value-of select="." />

mir-module/src/test/java/org/mycore/mir/xslt/XEditor2ModsXSLTest.java
@@ -1,0 +1,80 @@
+package org.mycore.mir.xslt;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.util.Map;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.Namespace;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mycore.common.util.MCRTestCaseClassificationUtil;
+import org.mycore.test.MCRJPAExtension;
+import org.mycore.test.MyCoReTest;
+
+@MyCoReTest
+@ExtendWith(MCRJPAExtension.class)
+public class XEditor2ModsXSLTest extends MIRXSLTFunctionTestCase {
+
+    private static final String XSL = "/xslt/editor/xeditor2mods.xsl";
+    private static final Namespace MCR_NAMESPACE = Namespace.getNamespace("mcr", "http://www.mycore.org/");
+    private static final Namespace XLINK_NAMESPACE = Namespace.getNamespace("xlink", "http://www.w3.org/1999/xlink");
+
+    @BeforeEach
+    void setUp() throws Exception {
+        MCRTestCaseClassificationUtil.addClassification("/classification/MIRTestClassification.xml");
+    }
+
+    @Test
+    void testPreservesClassificationBackedGenreWhenSaving() throws Exception {
+        Element genre = new Element("genre", MODS_NAMESPACE);
+        genre.setAttribute("type", "intern");
+        genre.setAttribute("categId", "MIRTestClassification:alpha", MCR_NAMESPACE);
+
+        Element mods = new Element("mods", MODS_NAMESPACE);
+        mods.addContent(genre);
+
+        Element modsContainer = new Element("modsContainer").addContent(mods);
+        Element defModsContainer = new Element("def.modsContainer").addContent(modsContainer);
+        Element metadata = new Element("metadata").addContent(defModsContainer);
+        Element mycoreobject = new Element("mycoreobject").addContent(metadata);
+
+        Document result = transformDocument(new Document(mycoreobject), XSL, Map.of());
+
+        Element savedGenre = result.getRootElement()
+            .getChild("metadata")
+            .getChild("def.modsContainer")
+            .getChild("modsContainer")
+            .getChild("mods", MODS_NAMESPACE)
+            .getChild("genre", MODS_NAMESPACE);
+
+        assertEquals("intern", savedGenre.getAttributeValue("type"));
+        assertEquals("http://www.mycore.org/classifications/MIRTestClassification",
+            savedGenre.getAttributeValue("authorityURI"));
+        assertEquals("http://www.mycore.org/classifications/MIRTestClassification#alpha",
+            savedGenre.getAttributeValue("valueURI"));
+        assertNull(savedGenre.getAttribute("categId", MCR_NAMESPACE));
+    }
+
+    @Test
+    void testRejectsMalformedHostObjectIdWhenBuildingStructure() throws Exception {
+        Element relatedItem = new Element("relatedItem", MODS_NAMESPACE);
+        relatedItem.setAttribute("type", "host");
+        relatedItem.setAttribute("href", "mir_mods_foo", XLINK_NAMESPACE);
+
+        Element mods = new Element("mods", MODS_NAMESPACE).addContent(relatedItem);
+        Element modsContainer = new Element("modsContainer").addContent(mods);
+        Element defModsContainer = new Element("def.modsContainer").addContent(modsContainer);
+        Element metadata = new Element("metadata").addContent(defModsContainer);
+        Element mycoreobject = new Element("mycoreobject");
+        mycoreobject.setAttribute("ID", "mir_mods_00000001");
+        mycoreobject.addContent(metadata);
+
+        Document result = transformDocument(new Document(mycoreobject), XSL, Map.of());
+
+        assertNull(result.getRootElement().getChild("structure"));
+    }
+}

mir-module/src/main/resources/xslt/functions/mireditorutils.xsl
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet version="3.0"
+  xmlns:mcrproperty="http://www.mycore.de/xslt/property"
   xmlns:mireditorutils="http://www.mycore.de/xslt/mireditorutils"
   xmlns:mods="http://www.loc.gov/mods/v3"
   xmlns:xs="http://www.w3.org/2001/XMLSchema"
@@ -14,7 +15,13 @@
   <xsl:function name="mireditorutils:is-valid-object-id" as="xs:boolean">
     <xsl:param name="value" as="xs:string?" />
     <xsl:variable name="normalized" select="normalize-space($value)" />
-    <xsl:sequence select="$normalized != '' and not(contains($normalized, ':')) and count(tokenize($normalized, '_')) = 3" />
+    <xsl:variable name="matches-pattern"
+      select="string-length($normalized) le 64
+              and matches($normalized, '^[A-Za-z][A-Za-z0-9]*_[A-Za-z0-9]+_[0-9]+$')" />
+    <xsl:variable name="type"
+      select="if ($matches-pattern) then lower-case(tokenize($normalized, '_')[2]) else ''" />
+    <xsl:sequence
+      select="$matches-pattern and mcrproperty:one(concat('MCR.Metadata.Type.', $type)) = 'true'" />
   </xsl:function>
 
   <xsl:function name="mireditorutils:build-extent-pages" as="element(mods:extent)">

mir-module/src/test/java/org/mycore/mir/xslt/MIREditorUtilsXSLTest.java
@@ -27,6 +27,8 @@ public class MIREditorUtilsXSLTest extends MIRXSLTFunctionTestCase {
     @Test
     void testIsValidObjectId() throws Exception {
         assertEquals("true", resultText(XSL, "test-is-valid-object-id", Map.of("value", "mir_mods_00000001")));
+        assertEquals("false", resultText(XSL, "test-is-valid-object-id", Map.of("value", "mir_mods_foo")));
+        assertEquals("false", resultText(XSL, "test-is-valid-object-id", Map.of("value", "mir_unknown_00000001")));
         assertEquals("false", resultText(XSL, "test-is-valid-object-id", Map.of("value", "mir:mods_00000001")));
         assertEquals("false", resultText(XSL, "test-is-valid-object-id", Map.of("value", "mir_mods")));
         assertEquals("false", resultText(XSL, "test-is-valid-object-id", Map.of("value", "")));

mir-module/src/test/java/org/mycore/mir/xslt/MIRXSLTFunctionTestCase.java
@@ -33,6 +33,10 @@ public abstract class MIRXSLTFunctionTestCase {
         return transform(prepareTestDocument(rootName, xml), xsl, parameters);
     }
 
+    protected Document transformDocument(Document xml, String xsl, Map<String, Object> parameters) throws Exception {
+        return transform(xml, xsl, parameters);
+    }
+
     protected String resultText(String xsl, String rootName, Map<String, Object> parameters) throws Exception {
         return transformRoot(xsl, rootName, parameters).getRootElement().getTextNormalize();
     }

commit c5d76b888ab51afd7234e6dea5ed4a9e20f3eb37

MIR-1540 fixed regressions from 06ff8fd93461c59ef9c142d0e3ae4d287df6a17e

mir-module/src/main/resources/xslt/editor/xeditor2mods.xsl
@@ -220,7 +220,7 @@
 
   <xsl:template match="@mcr:categId" />
   
-  <xsl:template match="mods:classification[@mcr:categId]">
+  <xsl:template match="mods:classification[@mcr:categId]" priority="2">
     <xsl:variable name="classid" select="mirmapper:classid(@mcr:categId)" />
     <xsl:variable name="categid" select="mirmapper:categid(@mcr:categId)" />
     <xsl:variable name="classURI" select="mirmapper:class-uri($classid)" />
@@ -235,7 +235,7 @@
     </mods:classification>
   </xsl:template>
 
-  <xsl:template match="mods:typeOfResource[@mcr:categId]">
+  <xsl:template match="mods:typeOfResource[@mcr:categId]" priority="2">
     <mods:typeOfResource>
       <xsl:copy-of select="@*[not(namespace-uri() = 'http://www.mycore.org/' and local-name() = 'categId')]" />
       <xsl:value-of select="replace(mirmapper:categid(@mcr:categId), '_', ' ')" />

mir-module/src/test/java/org/mycore/mir/xslt/XEditor2ModsXSLTest.java
@@ -60,6 +60,30 @@ public class XEditor2ModsXSLTest extends MIRXSLTFunctionTestCase {
     }
 
     @Test
+    void testRebuildsTypeOfResourceTextFromEditorCategoryBinding() throws Exception {
+        Element typeOfResource = new Element("typeOfResource", MODS_NAMESPACE);
+        typeOfResource.setAttribute("categId", "typeOfResource:still_image", MCR_NAMESPACE);
+
+        Element mods = new Element("mods", MODS_NAMESPACE).addContent(typeOfResource);
+        Element modsContainer = new Element("modsContainer").addContent(mods);
+        Element defModsContainer = new Element("def.modsContainer").addContent(modsContainer);
+        Element metadata = new Element("metadata").addContent(defModsContainer);
+        Element mycoreobject = new Element("mycoreobject").addContent(metadata);
+
+        Document result = transformDocument(new Document(mycoreobject), XSL, Map.of());
+
+        Element savedTypeOfResource = result.getRootElement()
+            .getChild("metadata")
+            .getChild("def.modsContainer")
+            .getChild("modsContainer")
+            .getChild("mods", MODS_NAMESPACE)
+            .getChild("typeOfResource", MODS_NAMESPACE);
+
+        assertEquals("still image", savedTypeOfResource.getTextNormalize());
+        assertNull(savedTypeOfResource.getAttribute("categId", MCR_NAMESPACE));
+    }
+
+    @Test
     void testRejectsMalformedHostObjectIdWhenBuildingStructure() throws Exception {
         Element relatedItem = new Element("relatedItem", MODS_NAMESPACE);
         relatedItem.setAttribute("type", "host");