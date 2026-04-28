28be9b4414c4d061019fac746ab46092c6d3e124
Lennard Golsch <lennard.golsch@icloud.com>
2026-04-28 17:15:25 +0200

    MIR-1572 fix OpenAIRE migration xslt

--- mir-module/src/test/java/org/mycore/mir/migration/MIRFundingMigrationTest.java
+++ mir-module/src/test/java/org/mycore/mir/migration/MIRFundingMigrationTest.java
@@ -1,75 +1,93 @@
+/*
+ * This file is part of ***  M y C o R e  ***
+ * See https://www.mycore.de/ for details.
+ *
+ * MyCoRe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyCoRe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyCoRe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.mycore.mir.migration;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.util.List;
 
-import org.jdom2.Attribute;
+import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
+import org.jdom2.Namespace;
+import org.jdom2.filter.Filters;
+import org.jdom2.xpath.XPathExpression;
+import org.jdom2.xpath.XPathFactory;
 import org.junit.jupiter.api.Test;
+import org.mycore.common.MCRConstants;
+import org.mycore.common.MCRException;
 import org.mycore.common.content.MCRContent;
-import org.mycore.common.content.MCRURLContent;
+import org.mycore.common.content.MCRStreamContent;
 import org.mycore.common.content.transformer.MCRContentTransformer;
 import org.mycore.common.content.transformer.MCRContentTransformerFactory;
-import org.mycore.mods.MCRMODSWrapper;
+import org.mycore.common.xml.MCRXMLHelper;
+import org.mycore.resource.MCRResourceHelper;
 import org.mycore.test.MyCoReTest;
 
 @MyCoReTest
 public class MIRFundingMigrationTest {
 
-    private static final String FUNDER_NAME = "European Commission";
-    private static final String FUNDER_ID = "https://doi.org/10.13039/501100000780";
-    private static final String FUNDER_ID_TYPE = "Crossref Funder ID";
-    private static final String AWARD_TITLE = "Towards climate-smart sustainable management of agricultural soils";
-    private static final String AWARD_NUMBER = "862695";
-    private static final String AWARD_URI = "https://cordis.europa.eu/project/id/" + AWARD_NUMBER;
+    private static final Namespace DATACITE_NAMESPACE
+        = Namespace.getNamespace("datacite", "http://datacite.org/schema/kernel-4");
 
-    private static final String FUNDING_REFERENCE_XPATH
-        = "mods:extension[@type='datacite-funding']/resource/fundingReferences/fundingReference";
-    private static final String OPEN_AIRE_IDENTIFIER_XPATH = "mods:identifier[@type='open-aire']";
-
-    private static final String FILE_PATH_VALID = "/openaire_mods.xml";
-    private static final String TRANSFORMER_ID = "migrate-openaire";
+    private static final String XPATH_DATACITE_FUNDING_EXTENSION
+        = "//mods:mods/mods:extension[@type='datacite-funding']";
 
     @Test
-    public void testMigrationTransformer_success() throws IOException, JDOMException {
-        final MCRContent input = loadContent();
-        final MCRContentTransformer transformer = MCRContentTransformerFactory.getTransformer(TRANSFORMER_ID);
-        final MCRContent result = transformer.transform(input);
+    public void shouldTransformValidInputToExpectedOutput() throws IOException, JDOMException {
+        MCRContent input = loadContent("input.xml");
+        MCRContentTransformer transformer = MCRContentTransformerFactory.getTransformer("migrate-openaire");
+        Document result = transformer.transform(input).asXML();
+        Document expected = loadContent("expected.xml").asXML();
 
-        final MCRMODSWrapper wrapper = new MCRMODSWrapper();
-        wrapper.setMODS(result.asXML().detachRootElement());
-
-        final Element identifierElement = wrapper.getElement(OPEN_AIRE_IDENTIFIER_XPATH);
-        assertNull(identifierElement);
-        final List<Element> fundingReferenceElements = wrapper.getElements(FUNDING_REFERENCE_XPATH);
-        assertEquals(1, fundingReferenceElements.size());
-        final Element fundingReferenceElement = fundingReferenceElements.get(0);
-        final Element awardTitleElement = fundingReferenceElement.getChild("awardTitle");
-        assertNotNull(awardTitleElement);
-        assertEquals(AWARD_TITLE, awardTitleElement.getText());
-        final Element funderName = fundingReferenceElement.getChild("funderName");
-        assertNotNull(funderName);
-        assertEquals(FUNDER_NAME, funderName.getText());
-        final Element funderIdentifierElement = fundingReferenceElement.getChild("funderIdentifier");
-        assertNotNull(funderIdentifierElement);
-        assertEquals(FUNDER_ID, funderIdentifierElement.getText());
-        final Attribute funderIdentifierTypeAttribute = funderIdentifierElement.getAttribute("funderIdentifierType");
-        assertNotNull(funderIdentifierTypeAttribute);
-        assertEquals(FUNDER_ID_TYPE, funderIdentifierTypeAttribute.getValue());
-        final Element awardNumberElement = fundingReferenceElement.getChild("awardNumber");
-        assertNotNull(awardNumberElement);
-        assertEquals(AWARD_NUMBER, awardNumberElement.getText());
-        final Attribute awardURIAttribute = awardNumberElement.getAttribute("awardURI");
-        assertNotNull(awardURIAttribute);
-        assertEquals(AWARD_URI, awardURIAttribute.getValue());
+        assertTrue(MCRXMLHelper.deepEqual(expected, result));
+        assertTrue(isCleanDataciteFundingExtension(result));
     }
 
-    private static MCRContent loadContent() {
-        return new MCRURLContent(MIRFundingMigrationTest.class.getResource(FILE_PATH_VALID));
+    @Test
+    public void shouldThrowExceptionForInvalidInput() {
+        final MCRContent input = loadContent("fail.xml");
+        final MCRContentTransformer transformer = MCRContentTransformerFactory.getTransformer("migrate-openaire");
+        assertThrows(MCRException.class, () -> transformer.transform(input).asXML());
     }
+
+    private static MCRContent loadContent(String name) {
+        return new MCRStreamContent(MCRResourceHelper.getResourceAsStream("/migration/funding/" + name));
+    }
+
+    private boolean isCleanDataciteFundingExtension(Document element) {
+        List<Element> extensions = getElements(element.getRootElement(), XPATH_DATACITE_FUNDING_EXTENSION);
+        if (extensions.size() != 1) {
+            return false;
+        }
+        return extensions.getFirst().getAdditionalNamespaces().isEmpty();
+    }
+
+    public List<Element> getElements(Element element, String xPath) {
+        return buildXPath(xPath).evaluate(element);
+    }
+
+    private XPathExpression<Element> buildXPath(String xPath) {
+        return XPathFactory.instance().compile(xPath, Filters.element(), null, MCRConstants.MODS_NAMESPACE,
+            MCRConstants.XLINK_NAMESPACE, DATACITE_NAMESPACE);
+    }
+
 }
